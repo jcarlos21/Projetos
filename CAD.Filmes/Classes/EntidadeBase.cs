ad90627df46149304e3be47ff93b677c19656e19
jcarlos21 <j.carlos2020@live.com>
2021-11-01 16:12:14 -0300

    Adicionando projeto Cadastro de Filmes

---
+++ CAD.Filmes/Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+namespace CAD.Filmes
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+    }
+}
---
+++ CAD.Filmes/README.md
@@ -0,0 +1,5 @@
+# Digital Innovation One - Prática .NET
+
+## Criando uma APP de cadastro de filmes em memória
+
+
