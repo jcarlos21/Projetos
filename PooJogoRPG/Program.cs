c86ded279d4bf20de0059b08b0b4eca96a8a4ab9
jcarlos21 <j.carlos2020@live.com>
2022-04-19 16:20:00 -0300

    commit de Jogo RPG em C# - Praticando orientacao a Objetos

---
+++ PooJogoRPG/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using PooJogoRPG.Entities;
+
+namespace PooJogoRPG {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+
+
+            
+        }
+    }
+}
