846a5ba130b30039dc6a1bea057452e512c3835f
jcarlos21 <j.carlos2020@live.com>
2022-04-26 11:30:43 -0300

    Atualizando

---
+++ SmartVendas/People.py
@@ -0,0 +1,39 @@
+class Person:
+    def __init__(self, name, gender, dateBirthday, address, zipCode, city, state, country):
+        self.name = name
+        self.gender = gender
+        self.dateBirthday = dateBirthday
+        self.address = address
+        self.zipCode = zipCode
+        self.city = city
+        self.state = state
+        self.country = country
+        
+    def insertDatabase(self):
+        # code implementation
+        pass
+
+class Employee (Person):
+    def __init__(self, dateAdmission, enrollment):
+        self.dateAdmission = dateAdmission
+        self.enrollment = enrollment
+
+    def insertDatabase(self):
+        # code implementation + method insertDatabase of the class Person
+        pass
+
+class Management (Employee):
+    def __init__(self, numberCRA):
+        self.numberCRA = numberCRA
+    
+    def insertDatabase(self):
+        # code implementation + method insertDatabase of the class Person and Employee
+        pass
+
+class Seller (Employee):
+    def __init__(self, departament):
+        self.departament = departament
+    
+    def insertDatabase(self):
+        # code implementation + method insertDatabase of the class Person and Employee
+        pass
