13f445a2f1f67924fd0531aca219c261b9071abd
jcarlos21 <j.carlos2020@live.com>
2022-04-29 23:06:22 -0300

    Atualizando

--- SmartVendas/People/People.py
+++ SmartVendas/People/People.py
@@ -11,15 +11,15 @@ class Person:
         self.city = city
         self.state = state
         self.country = country
-        
+    
     def insertDatabase(self):
         # code implementation to insert on database
         pass
 
 class Employee (Person):
-    def __init__(self, dateAdmission, enrollment):
+    def __init__(self, dateAdmission, enrollmentNumber):
         self.dateAdmission = dateAdmission
-        self.enrollment = enrollment
+        self.enrollmentNumber = enrollmentNumber
 
     def insertDatabase(self):
         # code implementation + method insertDatabase of the class Person to insert on database
